041b2e3d340893088c7387191a75719bffe8a5e7
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-15 19:30:19 +0200

    core modules implemented in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { HeaderComponent } from './core/components/header/header.component';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/product-list/product-list.module.ts
+++ src/app/pages/product-list/product-list.module.ts
@@ -1,5 +1,4 @@
 import { SharedModule } from './../../shared/shared.module';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
