71a883ecccb4eb5de8c32482aa8220b4aa84b991
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-16 20:14:51 +0200

    core module updated, fav cart component removed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ResponsiveNavComponent } from './components/header/components/responsive-nav/responsive-nav.component';
+import { NavBtnComponent } from './components/header/components/nav-btn/nav-btn.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ResponsiveNavComponent,
+    NavBtnComponent,
+  ],
   imports: [CommonModule, RouterModule],
   exports: [FooterComponent, HeaderComponent],
 })
