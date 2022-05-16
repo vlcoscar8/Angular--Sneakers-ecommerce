8b4ec3705129460dea162fbf490a887851624b33
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-16 13:48:39 +0200

    product list dinamic

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,6 +8,6 @@ import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, RouterModule],
 })
 export class HomeModule {}
--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { products } from './../../shared/components/product/config/product.config';
 import { IProduct } from './../../shared/components/product/model/product.model';
 import { Component, OnInit } from '@angular/core';
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  public products: IProduct[] = products as IProduct[];
+  public products?: IProduct[];
 
-  constructor() {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(async (e) => {
+      const genre = e['genre'];
+      const response = await fetch(
+        `https://sneakersecommerceapi.vercel.app//products?genre=${genre}`
+      );
+      this.products = await response.json();
+    });
+  }
 }
