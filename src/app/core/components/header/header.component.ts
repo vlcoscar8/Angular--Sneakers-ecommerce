fb5208866cabc8e0e0c767844b4c32b21281a2fd
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-16 19:43:08 +0200

    header button responsive

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { NavigationEnd, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   public path: string = '/';
+  public isDesktop?: boolean = false;
+  public navClicked?: boolean = false;
 
   constructor(private router: Router) {}
 
+  @HostListener('window:resize')
+  onResize() {
+    window.innerWidth > 500
+      ? (this.isDesktop = true)
+      : (this.isDesktop = false);
+  }
+
   ngOnInit(): void {}
 
-  public onClick = () => {
+  public onClick() {
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) {
         this.path = e.url.toString();
       }
     });
-  };
+  }
+
+  public btnClick() {
+    this.navClicked = !this.navClicked;
+  }
 }
