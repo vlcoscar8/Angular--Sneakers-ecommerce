530c3073d0f0ffac258bd4d3ad382b2a1a925bfb
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-19 23:04:47 +0200

    product list with pipe and swithcmap

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { ProductsService } from './../../core/services/product/products.service'
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from '../../core/services/product/model/product.model';
 import { Component, OnInit } from '@angular/core';
+import { switchMap, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -9,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  public products?: IProduct[];
+  public products?: Observable<IProduct[]>;
   public brand?: string;
   public genre?: string;
 
@@ -20,16 +21,16 @@ export class ProductListComponent implements OnInit {
 
   /**
    * Get the query params from the route ("genre" & "brand")
-   * then get the products list filtering by the querys
+   * then get the products list filtering by the querys usign the pipe with switchmap to concatenate asyncronus methods
    */
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(async (e) => {
-      this.genre = e['genre'];
-      e['brand'] ? (this.brand = e['brand']) : (this.brand = '');
+    this.products = this.activatedRoute.params.pipe(
+      switchMap((e) => {
+        this.genre = e['genre'];
+        e['brand'] ? (this.brand = e['brand']) : (this.brand = '');
 
-      this.productsService
-        .getProducts(this.genre, this.brand)
-        .subscribe((products) => (this.products = products));
-    });
+        return this.productsService.getProducts(this.genre, this.brand);
+      })
+    );
   }
 }
