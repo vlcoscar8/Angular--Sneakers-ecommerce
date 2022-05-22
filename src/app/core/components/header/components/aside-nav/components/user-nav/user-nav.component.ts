7ec9d8095a1f4b127cd6320a7aae676eb7423546
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-22 19:14:46 +0200

    user nav buttons styles

--- src/app/core/components/header/components/aside-nav/components/user-nav/user-nav.component.ts
+++ src/app/core/components/header/components/aside-nav/components/user-nav/user-nav.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-nav',
   templateUrl: './user-nav.component.html',
-  styleUrls: ['./user-nav.component.scss']
+  styleUrls: ['./user-nav.component.scss'],
 })
 export class UserNavComponent implements OnInit {
-
-  constructor() { }
+  public buttonClicked: string = '';
+  constructor() {}
 
   ngOnInit(): void {
+    this.buttonClicked = '';
   }
 
+  public openForm(value: any) {
+    this.buttonClicked = value.innerText;
+  }
+
+  public back() {
+    this.buttonClicked = '';
+  }
 }
